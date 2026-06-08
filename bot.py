f878dc1961aa28878b4053bfd95f1d6cac607cc7
EpicSenpai <arvindrathod5476@gmail.com>
2026-06-08 18:56:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -78,10 +78,7 @@ async def start(bot, cmd: Message):
             await add_user_to_database(bot, cmd)
         except:
             pass
-            
-        random_pic = random.choice(ANIME_IMAGES)
         
-        # Yeh raha tumhara exact text with Small Caps Font
         welcome_txt = (
             f"Hello, {cmd.from_user.first_name}\n\n"
             "This is a Permanent FileStore Bot.\n\n"
@@ -90,50 +87,17 @@ async def start(bot, cmd: Message):
             "⚠️ Benefits: If you have a Telegram Movie Channel or Any Copyright Channel Then This Bot Is Very Useful For You Because Your Channel Will Never Get Copyright Strike."
         )
         
-        # Small Caps Font Bypass
-        stylish_text = global_font_bypass(welcome_txt)
+        beautiful_text = f"**» ʜᴇʏ!!, {cmd.from_user.mention} ~ ❞**\n\n<blockquote>{global_font_bypass(welcome_txt)}</blockquote>"
         
-        # Vertical Quote (<blockquote>) aur Small Caps font ka combo
-        beautiful_text = f"**» ʜᴇʏ!!, {cmd.from_user.mention} ~ ❞**\n\n<blockquote>{stylish_text}</blockquote>"
-        
-        await bot.send_photo(
-            chat_id=cmd.chat.id,
-            photo=random_pic,
-            caption=beautiful_text,
-            reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("Updates Channel", url="https://t.me/+W0znQsN7HyAzNzUl")],
-                [InlineKeyboardButton("About Bot", callback_data="aboutbot"), InlineKeyboardButton("About Dev", callback_data="aboutdevs"), InlineKeyboardButton("Close 🚪", callback_data="closeMessage")],
-                [InlineKeyboardButton("Support Group", url="https://t.me/+fssm5SmO1uk5NjI9"), InlineKeyboardButton("YouTube Channel", url="https://youtube.com/@auratubeo")]
-            ])
-        )
-
-                    [
-                        InlineKeyboardButton("Updates Channel", url="https://t.me/+W0znQsN7HyAzNzUl")
-                    ],
-                    [
-                        InlineKeyboardButton("About Bot", callback_data="aboutbot"),
-                        InlineKeyboardButton("About Dev", callback_data="aboutdevs"),
-                        InlineKeyboardButton("Close 🚪", callback_data="closeMessage")
-                    ],
-                    [
-                        InlineKeyboardButton("Support Group", url="https://t.me/+fssm5SmO1uk5NjI9"),
-                        InlineKeyboardButton("YouTube Channel", url="https://youtube.com/@auratubeo")
-                    ]
-                ]
-            )
-        )
-
-                    [
-                        InlineKeyboardButton("Support Group", url="https://t.me/+fssm5SmO1uk5NjI9"),
-                        InlineKeyboardButton("YouTube Channel", url="https://youtube.com/@auratubeo")
-                    ]
-                ]
-            )
+        await cmd.reply_text(
+            text=beautiful_text,
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Updates Channel", url="https://t.me/+W0znQsN7HyAzNzUl")],[InlineKeyboardButton("About Bot", callback_data="aboutbot"), InlineKeyboardButton("About Dev", callback_data="aboutdevs"), InlineKeyboardButton("Close 🚪", callback_data="closeMessage")],[InlineKeyboardButton("Support Group", url="https://t.me/+fssm5SmO1uk5NjI9"), InlineKeyboardButton("YouTube Channel", url="https://youtube.com/@auratubeo")]])
         )
 
     else:
         try:
             try:
+
                 file_id = int(b64_to_str(usr_cmd).split("_")[-1])
             except (Error, UnicodeDecodeError):
                 file_id = int(usr_cmd.split("_")[-1])
