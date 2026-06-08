7bdf8df172260266f8c3588e4e8e331a8ba33295
EpicSenpai <arvindrathod5476@gmail.com>
2026-06-08 17:27:00 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -14,7 +14,7 @@ class Config(object):
   SHORTLINK_API = os.environ.get("SHORTLINK_API", "540e6d65d2851a9c645d0eafb573535af3d33943")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "6341576569"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://botuser:ShadowNode_2026@cluster0.gcku8pz.mongodb.net/?appName=Cluster0")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1003971704016")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002479514026")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003981196985"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
