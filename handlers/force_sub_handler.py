ba20576dfe2d72ba24fcb51faee4dc1dd255799a
EpicSenpai <arvindrathod5476@gmail.com>
2026-06-09 12:08:13 +0530

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -1,3 +1,8 @@
+async def get_invite_link(bot, chat_id):
+    # Dummy link taaki crash na ho agar back-end ise kahin call kare
+    return "https://t.me/"
+
 async def handle_force_sub(bot, cmd):
+    # Seedha 200 return karega taaki promotion ad bypass ho jaye
     return 200
     
